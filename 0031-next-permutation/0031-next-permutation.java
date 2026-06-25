1b222113723ae0af836a53aee870f2e7c2659fa9
ayushbansal-Cse <ayushbansal1209@gmail.com>
2026-06-26 02:31:29 +0530

    Time: 0 ms (100%), Space: 44.9 MB (15.19%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,28 +1,37 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        int  low=0;
-        int med=0;
-        int high=nums.length-1;
-        int temp;
-        while(med<=high){
-            if(nums[med]==0){
-                temp=nums[low];
-                nums[low]=nums[med];
-                nums[med]=temp;
-                med++;
-                low++;
+    public void nextPermutation(int[] nums) {
+        int n=nums.length-1;
+        int pivot=-1;
+        for(int i=n;i>0;i--){
+            if(nums[i]>nums[i-1]){
+                pivot=i-1;
+                break;
             }
-            else if(nums[med]==1){
-                med++;
-            }
-            else{
-                temp=nums[med];
-                nums[med]=nums[high];
-                nums[high]=temp;
-                high--;
-            }
-
         }
-    
+        if(pivot!=-1){
+        for(int i=n;i>pivot;i--){
+            if(nums[i]>nums[pivot]){
+                swap(nums,pivot,i);
+                break;
+            }
+        }
+        }
+        rev(nums,pivot+1,n);
     }
-}
+        public static void swap(int [] nums,int x,int y ) {
+            int temp;
+            temp=nums[x];
+            nums[x]=nums[y];
+            nums[y]=temp;
+        }
+        public static void rev(int []nums,int a,int b){
+            int temp;
+            while(a<=b){
+                temp=nums[a];
+                nums[a]=nums[b];
+                nums[b]=temp;
+                a++;
+                b--;
+            }
+        }
+    }
