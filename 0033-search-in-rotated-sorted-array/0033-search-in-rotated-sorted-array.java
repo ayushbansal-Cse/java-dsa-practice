162356f027339f93b7fb129eeb192cbea3f4f22e
ayushbansal-Cse <ayushbansal1209@gmail.com>
2026-06-28 02:03:19 +0530

    Time: 0 ms (100%), Space: 43.9 MB (46.12%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,43 +1,19 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int n = nums.length;
-        int pivot = findPivot(nums);
-
-        // Agar array rotated nahi hai
-        if (pivot == 0) {
-            return bs(nums, 0, n - 1, target);
-        }
-
-        // Agar target left half mein hai
-        if (target >= nums[0] && target <= nums[pivot - 1]) {
-            return bs(nums, 0, pivot - 1, target);
-        }
-
-        // Agar target right half mein hai
-        return bs(nums, pivot, n - 1, target);
-    }
-
-    // Pivot = smallest element index
-    private int findPivot(int[] nums) {
-        int low = 0, high = nums.length - 1;
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] > nums[high]) {
-                low = mid + 1;
-            } else {
-                high = mid;
+    public int search(int[] arr, int target) {
+        int n=arr.length;
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==target) return mid;
+            else if(arr[mid]<=arr[high]){
+                if(target>arr[mid] && arr[high]>=target) low=mid+1;
+                else high=mid-1;
             }
-        }
-        return low; // pivot index
-    }
-
-    // Normal binary search
-    private int bs(int[] arr, int low, int high, int target) {
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] == target) return mid;
-            else if (arr[mid] > target) high = mid - 1;
-            else low = mid + 1;
+            else {
+                if(target>=arr[low] && arr[mid]>target) high=mid-1;
+                else low=mid+1;
+            } 
         }
         return -1;
     }
