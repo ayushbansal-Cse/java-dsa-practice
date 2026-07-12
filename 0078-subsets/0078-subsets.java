d44e80c5214cb2614ca79918dc7c9aa6b1d345a3
ayushbansal-Cse <ayushbansal1209@gmail.com>
2026-07-13 01:55:25 +0530

    Time: 0 ms (100%), Space: 44.2 MB (40.02%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,24 +1,23 @@
 class Solution {
-    static List<List<Integer>> arr;
-    public static void set(int i,int[] nums,ArrayList<Integer> ans){
-        if(i==nums.length){
-            ArrayList<Integer> l=new ArrayList<>();
-            for(int j= 0;j<ans.size();j++){
-                l.add(ans.get(j));
+    static  List<List<Integer>> arr;
+    public static void set(int i,int [] nums,ArrayList<Integer> ans){
+        if(i== nums.length){
+            ArrayList<Integer> lst= new ArrayList<>();
+            for(int j=0;j<ans.size();j++){
+                lst.add(ans.get(j));
             }
-            arr.add(l);
+            arr.add(lst);
             return;
         }
-        
         set(i+1,nums,ans);
         ans.add(nums[i]);
         set(i+1,nums,ans);
         ans.remove(ans.size()-1);
     }
     public List<List<Integer>> subsets(int[] nums) {
-        arr =new ArrayList<>();
+        arr=new ArrayList<>();
         ArrayList<Integer> ans= new ArrayList<>();
         set(0,nums,ans);
         return arr;
-    } 
+    }
 }
